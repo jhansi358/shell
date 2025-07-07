13031193c27d64204d9a394271e3ae4a8c82db24
jhansirani <jhansirania358@gmail.com>
2025-07-07 10:22:36 +0530

    Update variable1.sh

--- variable1.sh
+++ variable1.sh
@@ -3,3 +3,9 @@ v1=hari
 v2=siri
 echo "$v1 : hi $v2"
 echo "$v2 : hi how are you $v1"
+#we can take values of variables while running via args
+v1=$1
+v2=$2
+echo "$v1 : hi $v2"
+echo "$v2 : hi how are you $v1"
+#run this using sh scriptname hari siri 
