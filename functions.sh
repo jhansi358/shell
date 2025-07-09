ba3ba63924b56978e9047fb53dbd4997fd76ebfa
jhansirani <jhansirania358@gmail.com>
2025-07-09 16:59:47 +0530

    fun

--- functions.sh
+++ functions.sh
@@ -3,25 +3,31 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log" # $0 GIVES SCRIPTNAME
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
 VALIDATE(){
 
 if [ "$?" -eq 0 ]
 then
-        echo " SUCCESSFUL INSTALLATION "
+        echo "$G SUCCESSFUL INSTALLATION $N"
 else
-        echo " ERROR.INSTALLATION FAILED"
+        echo "$R ERROR.INSTALLATION FAILED $N"
 fi
 }
 
 if [ "$ID" -ne 0 ]
 then
-        echo " ERROR:RUN WITH ROOT USER "
+        echo " $R ERROR:RUN WITH ROOT USER $N "
         exit 1
 else
-        echo " you are root user"
+        echo " $G you are root user $N"
 fi
 
-yum install mysql -y  &>>LOGFILE
+yum install mysql -y  &>>LOGFILE #&>> gives both success and failed logs into logfile
 
 VALIDATE
 
